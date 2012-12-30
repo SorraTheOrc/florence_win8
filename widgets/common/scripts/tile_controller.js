32bbbebc4cf2ef03cf5f1ad429888fc8ffbc1cb4
Ross Gardler <rgardler@opendirective.com>
2012-12-30 22:41:34 +0000

    move widget code into florence_widgets repo

--- widgets/common/scripts/tile_controller.js
+++
@@ -1,5 +0,0 @@
-$('#tile').live('pageinit',function(event) { 
-    $('#tile').click(function() {
-        window.widget.views.openUrl("http://florence.opendirective.net/widgets/${widget.shortname}", null, "_blank");
-    });
-});

479514dd0aa7174dd5e80d6b9aea830930b0e34a
Ross Gardler <rgardler@opendirective.com>
2013-01-07 00:19:31 +0000

    initial implementation of WidgetDataSource. This starts to move some of the hard coded stuff out of HTML and into Javascript. Next stage is to read the list of widgets from the file system.

---
+++ JS_Client/js/widgetDataSource.js
@@ -0,0 +1,25 @@
+﻿var WidgetDataSource = {
+    widgets: [],
+
+    //
+    // Get available widgets, and send
+    // to the callback function when retrieved
+    //
+    getWidgets: function(callback){
+        var widget = {
+            id: "http://florence.opendirective.net/widgets/audioPlayer",
+            name: "Audio Player",
+            src: "ms-appx-web:///widget/audioPlayer/index.html"
+        }
+        WidgetDataSource.widgets.push(widget);
+        
+        widget = {
+            id: "http://florence.opendirective.net/widgets/imagePlayer",
+            name: "Image Player",
+            src: "ms-appx-web:///widget/imagePlayer/index.html"
+        }
+        WidgetDataSource.widgets.push(widget);
+
+        callback(WidgetDataSource.widgets);
+    }
+}
